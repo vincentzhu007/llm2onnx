010c200b007c67ee03ef3fc14ecff714d65a6641
Vincent Zhu <VincentZhu007@gmail.com>
2025-03-09 01:06:58 +0800

    qwen: support run prefill stage of static onnx

--- qwen/qwen2onnx.py
+++ qwen/qwen2onnx.py
@@ -1,11 +1,11 @@
+import os
 import logging
 import argparse
 import numpy as np
 import torch
 import transformers
+import onnxruntime as ort
 from transformers import AutoModelForCausalLM, AutoTokenizer
-
-
 
 class Qwen2Onnx:
     def __init__(self):
@@ -29,15 +29,16 @@ class Qwen2Onnx:
             tokenize=False,
             add_generation_prompt=True
         )
-        input_ids = self.tokenizer([text], return_tensors="pt").to(self.model.device)
+        self.input_ids = self.tokenizer([text], return_tensors="pt").to(self.model.device)
 
         generated_ids = self.model.generate(
-            **input_ids,
+            **self.input_ids,
             max_new_tokens=256
             )
         return self.tokenizer.decode(generated_ids[0], skip_special_tokens=True)
 
     def export(self, model_path):
+        self.onnx_path = model_path
         torch.onnx.export(
             self.model,
             (torch.zeros(1, 256, dtype=torch.long),),
@@ -46,6 +47,25 @@ class Qwen2Onnx:
             output_names=['logits']
         )
 
+    def run_onnx(self, onnx_path):
+        self.ort_session = ort.InferenceSession(onnx_path)
+        input_name = self.ort_session.get_inputs()[0].name
+        output_name = self.ort_session.get_outputs()[0].name
+
+        input_data = np.zeros((1, 256), dtype=np.int64)
+        input_data.fill(self.tokenizer.eos_token_id)
+
+        real_input = self.input_ids['input_ids'].cpu().numpy()
+        input_data[0, :real_input.shape[1]] = real_input[0]
+
+        ort_inputs = {input_name: input_data}
+        ort_outputs = self.ort_session.run([output_name], ort_inputs)
+
+        id = np.argmax(ort_outputs[0][0][0])
+
+        return self.tokenizer.decode(id, skip_special_tokens=True)
+        
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
@@ -53,7 +73,9 @@ if __name__ == '__main__':
         description='export llm to various onnx.')
     
     parser.add_argument('-m', '--model', type=str, required=True, help='model path')
-    parser.add_argument('-o', '--output', type=str, required=False, default='qwen.onnx', help='output path')
+    parser.add_argument('-o', '--onnx', type=str, required=False, default='qwen.onnx', help='onnx path')
+    parser.add_argument('--force_export', action='store_true', help='force export onnx model, not use cache')
+
     args = parser.parse_args()
 
     logging.info(f'use model: {args.model}')
@@ -66,8 +88,14 @@ if __name__ == '__main__':
     logging.info(f'output: {text}')
     logging.info('run torch model done.\n')
 
+    if os.path.exists(args.onnx) and not args.force_export:
+        logging.info(f'onnx model exists: {args.onnx}, skip export.')
+    else:
+        logging.info('step 2: export onnx model...\n')
+        qwen2onnx.export(args.onnx)
+        logging.info(f'export onnx model done: {args.onnx}')
 
-    logging.info('step 2: export onnx model...\n')
-    qwen2onnx.export(args.output)
-    logging.info(f'export onnx model done: {args.output}')
-    
+    logging.info('step 3: run onnx model...\n')
+    text = qwen2onnx.run_onnx(args.onnx)
+    logging.info(f'output: {text}')
+    logging.info(f'run onnx model done.')    
